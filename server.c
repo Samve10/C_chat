d4f54b9e5995006ce234d82fd47220c9d0b1095d
Samve10 <samvel.a7@tumo.org>
2025-07-25 17:18:27 +0400

    dfwfew

--- server.c
+++ server.c
@@ -6,14 +6,14 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-
-#define PORT 1488
-#define MAX_CLIENTES 10
+	
+#define PORT 8080
+#define MAX_CLIENTS 10
 int main()
 {
 	int server_fd;
 	int new_client;
-	int client_sockets[MAX_CLIENTES];
+	int client_sockets[MAX_CLIENTS];
 	struct sockaddr_in address;
 	socklen_t addrlen = sizeof(address);
 	char buffer[960];
@@ -31,10 +31,60 @@ int main()
 	listen(server_fd, 5);
 	printf("Server started of port %d\n", PORT);
 	while(1)
-	{
+	{	
 		FD_ZERO(&readfds);
 		FD_SET(server_fd, &readfds);
 		int max_fd = server_fd;
+		for(int i = 0; i < MAX_CLIENTS; i++)
+		{
+			int fd = client_sockets[i];
+			if(fd > 0)
+			{
+				FD_SET(fd, &readfds);
+			}
+			if(fd > max_fd)
+			{
+				max_fd = fd;
+			}
+			select(max_fd + 1, &readfds, NULL, NULL, NULL);
+			if(FD_ISSET(server_fd, &readfds))
+			{
+				new_client = accept(server_fd, (struct sockaddr *) &address, &addrlen);
+				printf("New connection: socked fd: %d, IP: %s\n", new_client, inet_ntoa(address.sin_addr));
+			}
+			for(int i = 0; i < MAX_CLIENTS; i++)
+			{
+				if(client_sockets[i] == 0)
+				{
+					client_sockets[i] = new_client;
+					break;
+				}
+			}
+			for(int i = 0; i < MAX_CLIENTS; i++)
+			{
+				int val = read(fd, buffer, 1024);
+				if(val == 0)
+				{
+					getpeername(fd, (struct sockaddr *) &address, &addrlen);
+					printf("Client disconnected. IP: %s\n", inet_ntoa(address.sin_addr));
+					close(fd);
+					client_sockets[i] = 0;
+				}
+				else
+				{
+					buffer[val] = '\0';
+					for(int j = 0; j < MAX_CLIENTS; j++)
+					{
+						if(client_sockets[j] != 0 && client_sockets[j] != fd)
+						{
+							send(client_sockets[j], buffer, strlen(buffer), 0);
+						}
+					}
+
+					
+				}
+			}
+		}
 	}
 	return 0;
 }
